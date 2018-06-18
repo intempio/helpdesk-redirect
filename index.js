8ae197177c13fe7605475d5ce638358582e2ae06
Rustan Corpuz <rustanacexd@gmail.com>
2018-06-19 01:29:42 +0800

    update validation

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.get('/:lookupID', async (req, res, next) => {
       // check if required fields for this redirect are present.
       if (
         'event_link' in record.fields &&
-        'attendee_full_name' in record.fields
+        'attendee_full_name' in record.fields &&
+        record.fields.attendee_full_name[0] !== ' '
       ) {
         const acLink = record.fields.event_link[0];
         const encodedGuestName = encodeURIComponent(
@@ -39,10 +40,16 @@ app.get('/:lookupID', async (req, res, next) => {
         return res.redirect(`${acLink}?guestName=${encodedGuestName}`);
       }
 
-      return res.json({message: 'Ac link or Attendee Name fields missing.'});
+      return res
+        .status(500)
+        .send('Ac link or Attendee Name fields is missing.');
     }
 
-    return res.json({message: 'No Attendee with that link is found.'});
+    return res
+      .status(404)
+      .send(
+        'No Attendee with this link is found. Please double check the url.'
+      );
   } catch (error) {
     next(error);
   }
